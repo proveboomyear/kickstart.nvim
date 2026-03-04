289a5ee8e4eaae30bbf78346c078efbfdc8db9b5
proveboomyear <kiwi.exist.solar@clkdmail.com>
2026-03-03 22:23:19 -0500

    enabled relative and current number

--- init.lua
+++ init.lua
@@ -22,7 +22,11 @@ vim.g.have_nerd_font = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
+
+-- number on current line
+
+vim.opt.number = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = ''
